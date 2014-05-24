49b38b9e0d7a17247466cec89211f97f17a23dc5
Kim <kimpersson88@gmail.com>
2014-05-25 01:12:56 +0200

    Added all modules :(

--- routes/miner.js
+++ routes/miner.js
@@ -1,23 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var xgminer = require('xgminer');
+var async = require('async');
 
 var client = new xgminer('192.168.1.2', 4028);
 
-router.get('/status', function(req, res) {
-	var ret = {};
+
+var getSummary = function(callback) {
 	client.summary().then(function (data) {
-		console.log(data[ 'SUMMARY' ]);
-		ret.SUMMARY = data[ 'SUMMARY' ];
-		client.devs().then(function (data) {
-			ret.DEVS = data['DEVS'];
-			res.send(ret);
-		},function (err) {
-			console.error(err);
-		});
+		callback(null, data['SUMMARY']);
 	}, function (err) {
 		// an error occurred
 		console.error(err);
+	});
+}
+
+var getDevs = function(callback) {
+	client.devs().then(function (data) {
+		callback(null,data['DEVS']);
+	},function (err) {
+		callback(err,null);
+	});
+}
+
+router.get('/status', function(req, res) {
+	var ret = {};
+	async.parallel({
+			SUMMARY: getSummary,
+			DEVS: getDevs
+	},
+	function(err, results) {
+			// results is now equals to: {one: 1, two: 2}
+			console.log(done);
+			if(err)
+				console.error(err);
+			console.log(results);
 	});
 });
 module.exports = router;
