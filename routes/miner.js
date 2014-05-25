aae7e5208e7549eccbdc7a4fe113490f253fa0f8
Kim <kimpersson88@gmail.com>
2014-05-25 04:50:51 +0200

    Implemented plugin for getting miner status

--- routes/miner.js
+++ routes/miner.js
@@ -1,42 +1,15 @@
 var express = require('express');
+var miner_api = require('miner_api');
 var router = express.Router();
-var xgminer = require('xgminer');
-var async = require('async');
 
-var client = new xgminer('192.168.1.2', 4028);
-
-
-var getSummary = function(callback) {
-	client.summary().then(function (data) {
-		console.log("getDevs done");
-		callback(null, data['SUMMARY']);
-	}, function (err) {
-		// an error occurred
-		console.error(err);
-	});
-}
-
-var getDevs = function(callback) {
-	client.devs().then(function (data) {
-		console.log("getDevs done");
-		callback(null,data['DEVS']);
-	},function (err) {
-		callback(err,null);
-	});
-}
 
 router.get('/status', function(req, res) {
-	var ret = {};
-	async.parallel({
-			SUMMARY: getSummary,
-			DEVS: getDevs
-	},
-	function(err, results) {
-		// results is now equals to: {one: 1, two: 2}
-		console.log('Async done');
-		if(err)
+	miner_api.getStatus('192.168.1.2', 4028, function(err, results) {
+		if(err) {
 			console.error(err);
-		res.send(results);
+			res.send(500, "Error commuicating with miner")
+		} else
+			res.send(results);
 	});
 });
 module.exports = router;
